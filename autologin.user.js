5e57cafbdd8a841c77040bbd5a270962b849f59a
Samuel Phillips <sam@samuelphillips.net>
2016-04-26 20:27:49 -0400

    Added the main login page as well

--- autologin.user.js
+++ autologin.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @description This script will automatically press the login button
 // @match https://mymasonportal.gmu.edu/webapps/*
+// @match https://mymasonportal.gmu.edu/webapps/login/
 // @name bb-autologin
 // @namespace https://www.samuelphillips.net/gmscripts
 // @run-at document-idle
 // @version 1.0.0
 // ==/UserScript==
 
-var form = document.querySelector('form[target="_top"][name="login"]' +
-        '[action="/webapps/login/"');
+var form = document.querySelector('form[name="login"]' +
+        '[action="/webapps/login/"]');
 if (form) {
     console.log("BB-AUTOLOGIN: Pushing the button now!");
     form.submit();
